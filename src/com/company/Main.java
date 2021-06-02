0f0bb5d7cc4f88cb67117fa30a1436249a3c4e55
helendansh <danshinahel@yandex.ru>
2021-06-02 15:44:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.*;
+import java.time.Instant;
+import java.time.Duration;
+
+public class Main {
+
+    public static void main(String[] args) {
+    List<Integer> arlist = new ArrayList<>();
+    List<Integer> linklist = new LinkedList<>();
+    System.out.println("Прошло времени в ArrayList, мс: " + countTime(addlist(arlist)));
+    System.out.println("Прошло времени в LinkedList, мс: " + countTime(addlist(linklist)));
+    }
+    public static List<Integer> addlist (List<Integer> list) {
+        for (int i = 0; i < 1000; i++) {
+            list.add(i);
+        }
+        return list;
+    }
+    public static void getlist (List<Integer> list) {
+       for (int i=0; i < 1000; i++) {
+            list.get(i);
+        }
+    }
+    public static long countTime (List<Integer> list) {
+        Instant start = Instant.now();
+        getlist(list);
+        Instant finish = Instant.now();
+        return Duration.between(start, finish).toMillis();
+    }
+}
